b09f6752ac6baa1c622e3f794b9a87928642ed2c
Iwanaga-Sena <iwanaga.sena.infratop@gmail.com>
2024-08-13 09:33:15 +0000

    [add] devise and name culumn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "2aff8793946743b3a9bd54059aa9145e.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
